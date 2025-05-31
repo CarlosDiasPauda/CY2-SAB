a067e243a93f938de6fe0655ba6326e540a806e5
CarlosDiasPauda <cadudipauda@gmail.com>
2025-05-31 10:25:16 -0300

    Aulão do dia 31

---
+++ aula13/script.js
@@ -0,0 +1,47 @@
+// variavel = [valores] listas em python
+
+// var -> ngm usa pq não inicializa automaticamente
+// let -> menos problematica e mais utilizada
+// const -> variaveis de leitura e apenas de leita(não alteraveis)
+// == (comparar);
+// === (comparar para os paremetro iguais);
+// != ()
+
+// let listaLetras = ['a', 'b', 'c', 'd', 'e'];
+// let listaNumeros = [1, 2, 3, 4, 5];
+// listaNumeros.unshift(0);
+// listaNumeros.splice(3, 0, '2.5');
+// listaNumeros.push(6);
+// listaNumeros.unshift(listaLetras);
+// delete listaNumeros[7];
+// document.writeln(listaNumeros);
+// let temperatura = Math.random(5, 40);
+
+// let temperatura = prompt("Digite um numero");
+// if (temperatura > 35){
+//     document.writeln("Está muito Quente.");
+// } else if (temperatura > 25){
+//     document.writeln("Está agradavel.");
+// } else {
+//     document.writeln("Está frio.");
+// }
+
+// for(let contador = 0; contador <= 100; contador++){
+//     document.writeln(contador);
+// }
+
+// let nomes = ["manoel", "carlos", "diogo"];
+// for(let nome in nomes){
+//     document.writeln(nome+':'+ nomes[nome]);
+// }//percorre o indice (in: nome=indice)
+
+// for(let nome of nomes){
+//     document.writeln(nome);
+// }//percorre valores (for: nome=valores)
+
+let senha = '';
+while(senha != 'abc123'){
+    senha = prompt('digite a senha');
+} 
+
+document.writeln('ta podendo👍🔥🔥🔥🔥')
